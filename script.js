1ef5d8345f203b6723ceb72e298d655e5084015f
Your Name <nine.jp.4nft@gmail.com>
2025-08-07 11:20:24 +0900

    WIP: 現在地ボタン追加

--- script.js
+++ script.js
@@ -76,6 +76,28 @@ function initMap() {
 
     addMarkers();
     updateProgress();
+
+    // 現在地ボタンのイベントリスナー
+    document.getElementById('current-location-button').addEventListener('click', () => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    const pos = {
+                        lat: position.coords.latitude,
+                        lng: position.coords.longitude,
+                    };
+                    map.setCenter(pos);
+                    map.setZoom(16); // ズームレベルを調整
+                },
+                () => {
+                    handleLocationError(true, infoWindow, map.getCenter());
+                }
+            );
+        } else {
+            // Browser doesn't support Geolocation
+            handleLocationError(false, infoWindow, map.getCenter());
+        }
+    });
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {

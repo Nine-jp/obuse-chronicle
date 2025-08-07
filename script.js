1040e286c8fe95ec47ae45b65a9622a20bbee268
Your Name <nine.jp.4nft@gmail.com>
2025-08-07 13:59:29 +0900

    Fix: Suppress location error message

--- script.js
+++ script.js
@@ -102,12 +102,8 @@ function initMap() {
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.setPosition(pos);
-    infoWindow.setContent(
-        browserHasGeolocation
-            ? "エラー: 位置情報サービスに失敗しました。"
-            : "エラー: お使いのブラウザは位置情報に対応していません。"
-    );
-    infoWindow.open(map);
+    
+    
 }
 
 // マーカーの追加
